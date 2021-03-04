02b773f329f77aa463df54c9314243a45516305a
kento-hasegawa <hasegawa.k0586@gmail.com>
2021-03-04 23:03:38 +0900

    vue animation

--- laravel-vue-app/routes/web.php
+++ laravel-vue-app/routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', 'homeController@home')->name('home');
 Route::get('/index', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index','show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
+
+Route::prefix('articles')->name('articles.')->group(function () {
+    Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+    Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+});
