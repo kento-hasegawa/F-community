3ffe1c9fe550b788093d3e4186bdaf26c611dec7
kento-hasegawa <hasegawa.k0586@gmail.com>
2021-03-09 23:36:32 +0900

    login add

--- laravel-vue-app/routes/web.php
+++ laravel-vue-app/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function () {
+    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
+});
 Route::get('/', 'homeController@home')->name('home');
 Route::get('/index', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index','show'])->middleware('auth');
